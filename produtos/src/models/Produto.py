4423812a5534abdfa55dbef1281c2eb5f6ae5e25
Leonardo Vizagre <leonardo.vizagre@avanade.com>
2026-04-22 16:45:10 -0300

    Melhorias na classe de produtos

--- produtos/src/models/Produto.py
+++ produtos/src/models/Produto.py
@@ -1,22 +1,46 @@
-import datetime
-from abc import abstractmethod, ABC
+from __future__ import annotations
 
-class Produto(ABC):
+class Produto():
     """
         Classe que representa um produto.
+        Atributos:
+            id (int): O identificador único do produto.
+                id_ -> parâmetro do construtor
+                _id -> atributo privado
+
+            nome (str): O nome do produto.
+            preco (float): O preço do produto.
+                _preco -> atributo privado
+            ativo (bool): Indica se o produto está ativo ou não.
     """
 
-    def __init__(self, id: int, nome: str, preco: float, data_criacao: datetime.date, ativo: bool = True) -> None:
-        self.id :int = id
+    def __init__(self, id_: int, nome: str, preco: float, ativo: bool = True) -> None:
+        self._id :int = id_
         self.nome :str = nome
-        self.preco :float  = preco
-        self.data_criacao :datetime.date = data_criacao
+        self._preco :float  = 0.00
+        self.preco = preco
         self.ativo :bool = ativo
 
 
     def __str__(self) -> str:
-        return f"Produto(id={self.id}, nome='{self.nome}', preco={self.preco}, data_criacao={self.data_criacao}, ativo={self.ativo})" 
-    
+        return f"Produto(id={self._id}, nome='{self.nome}', preco={self._preco}, ativo={self.ativo})" 
+
+
+    @property
+    def id(self) -> int:   
+        return self._id
+
+
+    @property
+    def preco(self) -> float:
+        return self._preco
+
+    @preco.setter
+    def preco(self, valor: float) -> None:
+        if valor < 0:
+            raise ValueError("O preço não pode ser negativo.")
+        self._preco = valor
+
 
     def aplicar_desconto(self, percentual: float) -> None:
         """
@@ -30,4 +54,13 @@ class Produto(ABC):
         desconto = self.preco * (percentual / 100)
         self.preco -= desconto
 
+
+    def ativar(self) -> None:
+        """Ativa o produto."""
+        self.ativo = True   
+
+
+    def desativar(self) -> None:
+        """Desativa o produto."""
+        self.ativo = False      
 
